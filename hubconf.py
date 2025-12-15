a9ed8384d320c017f6532ab92240d5c673881217
Hadrien Crassous <crassoushadrien@gmail.com>
2025-12-15 14:08:18 +0100

    removed select device which defaults to cuda:0

--- hubconf.py
+++ hubconf.py
@@ -37,7 +37,7 @@ def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbo
     save_dir = Path('') if str(name).endswith('.pt') else file.parent
     path = (save_dir / name).with_suffix('.pt')  # checkpoint path
     try:
-        device = select_device(('0' if torch.cuda.is_available() else 'cpu') if device is None else device)
+        # device = select_device(('0' if torch.cuda.is_available() else 'cpu') if device is None else device)
 
         if pretrained and channels == 3 and classes == 80:
             model = attempt_load(path, map_location=device)  # download/load FP32 model
