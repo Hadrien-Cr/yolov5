7683a7fdfeb2a064ff4220e60e9107e567141c3f
Crassous Hadrien <crassoushadrien@gmail.com>
2026-02-03 08:49:46 +0100

    edited min area forward

--- utils/general.py
+++ utils/general.py
@@ -643,7 +643,7 @@ def non_max_suppression(
     labels=(),
     max_det=300,
     return_full=False,
-    min_area=10*10,
+    min_area=32*32,
 ):
     """Runs Non-Maximum Suppression (NMS) on inference results
 
